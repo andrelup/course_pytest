2e52a2da1f514d956061e8c933f269bbb3d44dd7
AndreCostaLIS <andreluis.costa@lisdatasolutions.com>
2026-02-18 13:18:48 +0100

    Add additional product CRUD tests; implement successful update and patch tests, and handle not found case

--- tests/integration/test_product.py
+++ tests/integration/test_product.py
@@ -20,3 +20,34 @@ class TestCrudProduct:
                 response.json()["name"] == fake_product.name and
                 response.json()["price"] == fake_product.price
         )
+
+    async def test_delete_product_successful(self, async_client, fake_product):
+        response = await async_client.delete(f"{URL_PRODUCTOS}{fake_product.id}")
+        assert (response.status_code == status.HTTP_200_OK and
+                response.json()["id"] == fake_product.id and
+                response.json()["name"] == fake_product.name and
+                response.json()["price"] == fake_product.price
+        )
+
+    async def test_update_product_successful(self, async_client, fake_product):
+        payload = {"name": "Prueba Update", "price": 300}
+        response = await async_client.put(f"{URL_PRODUCTOS}{fake_product.id}", json=payload)
+        assert (response.status_code == status.HTTP_200_OK and
+                response.json()["id"] == fake_product.id and
+                response.json()["name"] == payload["name"] and
+                response.json()["price"] == payload["price"]
+        )   
+
+    async def test_patch_product_successful(self, async_client, fake_product):
+        payload = {"price": 400}
+        response = await async_client.patch(f"{URL_PRODUCTOS}{fake_product.id}", json=payload)
+        assert (response.status_code == status.HTTP_200_OK and
+                response.json()["id"] == fake_product.id and
+                response.json()["name"] == fake_product.name and
+                response.json()["price"] == payload["price"]
+        )
+
+    async def test_patch_product_not_found(self, async_client):
+        payload = {"price": 400}
+        response = await async_client.patch(f"{URL_PRODUCTOS}9999", json=payload)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
